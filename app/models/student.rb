1149fdb34f7d3fac4c44dd28ccfd71e994531246
salarkhan <salar.k3@gmail.com>
2013-10-24 23:13:03 -0700

    This is the solution to DB Drill- AR Student Schema.  All the validations pass :)

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,17 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :email, :uniqueness => true
+  validates :age, :numericality => {:greater_than => 5 }
+  validates :phone, :format => {:with => /.*\d{3}.*\d{3}.*\d{4}.*/}
+
+  def name 
+    first_name + " " + last_name
+  end
+
+  def age
+    Time.now.year - birthday.year
+  end
+
 end
